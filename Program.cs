b66a503d79f8de5117fd1b0cb2f301861ec26f67
MarkusBercel <markusb@kkszki.hu>
2024-10-11 11:54:10 +0200

    Project is ready

--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@ namespace MarkusBnapszak
 {
     internal class Program
     {
+        static void udvozles()
+        {
+
+            string reggel = "Jó reggelt";
+            string del = "Jó délt";
+            string delutan = "Jó délutánt";
+            string este = "Jó estét";
+            string hajnal = "Jó hajnalt";
+            Console.WriteLine(DateTime.Now);
+
+            int napszak = DateTime.Now.Hour + 12;
+            if (napszak >= 6 && napszak <= 12) Console.WriteLine(reggel);
+            if (napszak == 12) Console.WriteLine(del);
+            if (napszak > 12 && napszak <= 18) Console.WriteLine(delutan);
+            if (napszak > 18 && napszak <= 24) Console.WriteLine(este);
+            if (napszak > 0 && napszak < 6) Console.WriteLine(hajnal);
+        }
         static void Main(string[] args)
         {
+            udvozles();
+
         }
     }
 }
